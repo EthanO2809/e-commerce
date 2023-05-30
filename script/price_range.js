9b1c2434c5a27ba25283e3d5e45e2946546a364e
Ethan Oliver <ethanliamoliver28@gmail.com>
2023-05-30 16:34:54 +0200

    e-commerce

--- script/price_range.js
+++ script/price_range.js
@@ -1,69 +1,74 @@
 let Products = [
-    {
-        name1: '62inch 8K UHD LG OLED TV  - R18 999',
-        name2: "Xbox Series S 1TB  - R11 999",
-        name3: "500GB Xbox One S  - R4 499",
-        name4: 'Phoenix H200-i5k Gaming PC <BR CLEAR="left"/>– Z590/i5-  11600k/16GB/RTX3060/512SSD/750W  - R 12 999',
-        name5: "Gaming Monitor Png - Asus 35 Rog Strix Xg35vq  - R 7 999",
-        name6: "Brother DCP-T720W 3-in-1 Wi-Fi Refill Inkjet Printer  - R3 499",
-        name7: "Sony PlayStation 5 1TB+Controller  - R14 999",
-        name8: "Xbox One Controller (Red)  - R1 399",
-        name9: "Loudspeaker Wireless speaker JBL Boombox Audio  - R1 399",
-        image1: "./content/pngegg (2).png",
-        image2: "./content/pngimg.com - xbox_PNG101377.png",
-        image3: "./content/pngegg (1).png",
-        image4: "./content/thermaltake-h200tg-pc-gaming-case-south-africa.png",
-        image5: "./content/pngegg.png",
-        image6: "./content/pngwing.com (1).png",
-        image7: "./content/[CITYPNG.COM]Ps5 Official Console And Controller - 1824x1302.png",
-        image8: "./content/pngwing.com.png",
-        image9: "./content/pngwing.com (2).png",
-    }
+  {
+    image: "./content/pngegg (2).png",
+    Name: '62inch 8K UHD LG OLED TV',
+    Price: "- R18 999"
+  },
+  {
+    image: "./content/pngimg.com - xbox_PNG101377.png",
+    Name: "Xbox Series X 1TB",
+    Price: "- R12 499"
+  },
+  {
+    image: "./content/pngegg (1).png",
+    Name: '500GB Xbox One S',
+    Price: "- R4 499"
+  },
+  {
+    image: "./content/thermaltake-h200tg-pc-gaming-case-south-africa.png",
+    Name: 'Phoenix H200-i5k Gaming PC <BR CLEAR="left"/> – Z590/i5-  11600k/16GB/RTX3060/512SSD/750W',
+    Price: "- R12 999"
+  },
+  {
+    image: "./content/pngegg.png",
+    Name: 'Gaming Monitor Png - Asus 35 Rog Strix Xg35vq',
+    Price: "- R 7 999"
+  },
+  {
+    image: "./content/pngwing.com (1).png",
+    Name: 'Brother DCP-T720W 3-in-1 Wi-Fi Refill Inkjet Printer',
+    Price: "- R3 499"
+  },
+  {
+    image: "./content/[CITYPNG.COM]Ps5 Official Console And Controller - 1824x1302.png",
+    Name: 'Sony PlayStation 5 1TB+Controller',
+    Price: "- R14 999"
+  },
+  {
+    image: "./content/pngwing.com.png",
+    Name: 'Xbox One Controller (Red)',
+    Price: "- R1 399"
+  },
+  {
+    image: "./content/pngwing.com (2).png",
+    Name: 'Loudspeaker Wireless speaker JBL Boombox Audio',
+    Price: "- R1 399"
+  },
+  {
+    image: "./content/pngegg (3).png",
+    Name: 'MSI VIGOR GK80 RED Keyboard Cherry MSI Vigor GK80 RGB Mechanical Gaming Keyboard',
+    Price: "- R1 299"
+  },
+  {
+    image: "./content/pngegg (4).png",
+    Name: 'ROG STRIX SCAR Edition Gaming Laptop GL503',
+    Price: "- R14 799"
+  },
+  {
+    image: "./content/pngegg (5).png",
+    Name: 'XPERT-H2 Gaming-Headset',
+    Price: "- R899"
+  },
 ];
 
-
-let dispProducts = document.getElementById('Products');
+let dispProducts = document.querySelector('#Products.row');
 Products.forEach((data) => {
-    dispProducts.innerHTML += `<div class="container-fluid bg-secondary-subtle rounded-4 text-center flex" id="products">
+    dispProducts.innerHTML += `<div class="container-fluid bg-secondary-subtle rounded-4 text-center flex row col-12" id="products">
     <p class="text-center">
-        <a href=""><img src="${data.image1}" alt="" class="imgs"/><br>
-          ${data.name1}
+        <img src="${data.image}" alt="" class="imgs img-fluid"/><BR CLEAR="left"/>
+        <a href="" class="blck">${data.Name}<BR CLEAR="left"/>${data.Price}
       </a></p><br>
-    <p class="text-center">
-        <a href=""><img src="${data.image2}" alt="" class="imgs"/><br>
-        ${data.name2}
-      </a></p><br>
-      <p class="text-center">
-        <a href=""><img src="${data.image3}" alt="" class="imgs"/><br>
-        ${data.name3}
-      </a></p><br>
-      <br>
- <p class="text-center">
-  <a href=""><img src="${data.image4}" alt="" class="imgs"/><br>
-  ${data.name4}
-</a></p>
-<br>
- <p class="text-center">
-  <a href=""><img src="${data.image5}" alt="" class="imgs"/><br>
-  ${data.name5}
-</a></p><br>
-<p class="text-center">
-    <a href=""><img src="${data.image6}" alt="" class="imgs"/><br>
-    ${data.name6}
-  </a></p><br>
- <p class="text-center">
-  <a href=""><img src="${data.image7}" alt="" class="imgs"/><br>
-  ${data.name7}
-</a></p><br>
- <p class="text-center">
-  <a href=""><img src="${data.image8}" alt="" class="imgs"/><br>
-  ${data.name8}
-</a></p><br>
- <p class="text-center">
-  <a href=""><img src="${data.image9}" alt="" class="imgs"/><br>
-  ${data.name9}
-</a></p><br>
- 
+  
   </div>
   `;
 });
--- script/scripting.js
+++ script/scripting.js
@@ -1,6 +1,6 @@
 let Products = [
     {
-        name1: "Phoenix H200-i5k Gaming PC – Z590/i5-11600k/16GB/RTX3060/512SSD/750W",
+        name1: 'Phoenix H200-i5k Gaming PC <BR CLEAR="left"/> – Z590/i5-  11600k/16GB/RTX3060/512SSD/750W',
         name2: "Gaming Monitor Png - Asus 35 Rog Strix Xg35vq",
         name3: "500GB Xbox One S",
         name4: "62inch 8K UHD LG OLED TV",
@@ -20,21 +20,21 @@ Products.forEach((data) => {
       <br><h1 class="fs-3 text-black h2">Top Picks</h1>
       <br>
      <p class="text-center">
-      <a href=""><img src="${data.image1}" alt="" width="300rem" /><br>
+      <a href=""><img src="${data.image1}" alt="" width="300rem" class="img-fluid" loading="lazy"/><br>
       ${data.name1}
     </a></p>
 <br>
      <p class="text-center">
-      <a href=""><img src="${data.image2}" alt="" width="300rem" /><br>
+      <a href=""><img src="${data.image2}" alt="" width="300rem" class="img-fluid" loading="lazy"/><br>
       ${data.name2}
     </a></p><br>
 
      <p class="text-center">
-      <a href=""><img src="${data.image3}" alt="" width="300rem" /><br>
+      <a href=""><img src="${data.image3}" alt="" width="300rem" class="img-fluid" loading="lazy"/><br>
       ${data.name3}
     </a></p><br>
      <p class="text-center">
-      <a href=""><img src="${data.image4}" alt="" width="300rem" /><br>
+      <a href=""><img src="${data.image4}" alt="" width="300rem" class="img-fluid" loading="lazy"/><br>
       ${data.name4}
     </a></p><br>
       
